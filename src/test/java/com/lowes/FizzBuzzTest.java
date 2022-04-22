472d48017aee7dff7029b4501ac4e8801c6f571c
Farokh Eduljee <farokh.eduljee@lowes.com>
2022-04-22 15:56:09 -0400

    Initial commit

---
+++ src/test/java/com/lowes/FizzBuzzTest.java
@@ -0,0 +1,35 @@
+package com.lowes;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FizzBuzzTest {
+    @Test
+    void shouldReturnInputValueAsString(){
+        assertEquals("1", new FizzBuzz().convert(1));
+        assertEquals("2", new FizzBuzz().convert(2));
+    }
+
+    @Test
+    void shouldReturnFizz(){
+        assertEquals("Fizz", new FizzBuzz().convert(3));
+    }
+
+    @Test
+    void shouldReturnBuzz(){
+        assertEquals("Buzz", new FizzBuzz().convert(5));
+    }
+
+    @Test
+    void shouldReturnFizzBuzz(){
+        assertEquals("FizzBuzz", new FizzBuzz().convert(15));
+    }
+
+    @Test
+    void visualVerification(){
+        for(int i=1; i <= 100; i++) {
+            System.out.println(new FizzBuzz().convert(i));
+        }
+            
+    }
+}
